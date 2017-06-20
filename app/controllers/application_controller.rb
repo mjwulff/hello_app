11f5dbdb8baed43af233e33e979db6c81549a853
mjwulff <mjwulff625@gmail.com>
2017-06-19 20:47:08 -0400

    different hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-	render html: "hola, mundo!"
+	render html: "Hello World, this is Mike and I think I get this stuff!"
   end
   def goodbye
        render html: "adios !"
